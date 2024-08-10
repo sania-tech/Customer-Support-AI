37608fa01052efa11e0e522703bcdfc7ba7fa874
Sania-tech <saniasohail782@gmail.com>
2024-08-10 18:39:12 +0300

    https://github.com/sania-tech/Customer-Support-AI.git

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   const [messages, setMessages] = useState([
     {
       role: 'assistant',
-      content: 'Hi, I’m the Headstarter Support Agent. How can I assist you today?',
+      content: 'Hi, I’m your Personal ChatBot. How can I assist you today?',
     }
   ]);
 
